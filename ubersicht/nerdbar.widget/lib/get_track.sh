fbda617e4bf4f3b0b2a95d439459e89f795b4588
Jens Frost <jfl@lakeside.dk>
2016-11-09 13:53:54 +0100

    Revert "Ubersicht: fix in lib/get_track.sh"
    This reverts commit 90a9a1ac1232f5745ebf65c89c3d20a9dc060819.

--- ubersicht/nerdbar.widget/lib/get_track.sh
+++ ubersicht/nerdbar.widget/lib/get_track.sh
@@ -3,7 +3,7 @@
 check=`ps -ef | grep "/usr/local/bin/mopidy" | grep -v "grep" | wc -l | cut -d " " -f8`
 
 if [ $check -eq 1 ]; then
-   track_string=$(/usr/local/bin/mpc current --format '%artist% - %title%')
+   track_string=(/usr/local/bin/mpc current --format '%artist% - %title%')
    echo $track_string
 else
     echo ""
