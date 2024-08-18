03510dc30a03709352f071d950526bd0e593c01c
Jens Frost <jens.frost.lunding@vippsmobilepay.com>
2024-08-18 19:45:21 +0200

    wezterm upd

--- wezterm/wezterm.lua
+++ wezterm/wezterm.lua
@@ -8,13 +8,13 @@ local config = wezterm.config_builder()
 
 -- For example, changing the color scheme:
 config.color_scheme = 'Smyck'
-config.window_background_opacity = 0.8
+config.window_background_opacity = 0.95
 config.macos_window_background_blur = 10
 
 config.font = wezterm.font("MesloLGS Nerd Font Mono")
 config.font_size = 13
 
-config.enable_tab_bar = false
+config.enable_tab_bar = true
 
 -- and finally, return the configuration to wezterm
 return config
