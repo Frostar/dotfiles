a9787c0cd350deb0b2bad77f7d18bb22ebcd90d9
Jens Frost <post@j-frost.dk>
2026-01-28 19:06:52 +0100

    Update nvim: remove copilot

--- nvim/lua/plugins/init.lua
+++ nvim/lua/plugins/init.lua
@@ -33,25 +33,4 @@ return {
   	  },
   	},
   },
-
-  --  -- Enable Copilot
-  -- {
-  --   "github/copilot.vim",
-  --   lazy = false,
-  --   config = function()
-  --     vim.g.copilot_no_tab_map = true
-  --     vim.g.copilot_assume_mapped = true
-  --   end
-  -- }
-  {
-    "zbirenbaum/copilot.lua",
-    cmd = "Copilot",
-    event = "InsertEnter",
-  },
-  {
-    "zbirenbaum/copilot-cmp",
-    config = function()
-      require("copilot_cmp").setup()
-    end,
-  },
 }
