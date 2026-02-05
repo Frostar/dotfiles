c9afe297af201902cad3b3a93e6f79b378b77784
Jens Frost <jens.frost.lunding@vippsmobilepay.com>
2026-02-05 18:40:28 +0100

    Updated nvim

--- nvim/lua/options.lua
+++ nvim/lua/options.lua
@@ -14,3 +14,38 @@ o.smarttab = true
 o.expandtab = true
 
 o.incsearch = true
+
+-- Whitespace rendering
+o.list = true
+vim.opt.listchars = {
+  space = '·',
+  tab = '→ ',
+  trail = '·',
+  extends = '›',
+  precedes = '‹',
+  nbsp = '␣',
+}
+
+-- Auto-reload files changed outside of Neovim
+o.autoread = true
+vim.api.nvim_create_autocmd({ "FocusGained", "BufEnter", "CursorHold", "CursorHoldI" }, {
+  command = "if mode() != 'c' | checktime | endif",
+})
+
+-- Open NvimTree when launching without a file
+vim.api.nvim_create_autocmd("VimEnter", {
+  callback = function()
+    if vim.fn.argc() == 0 and vim.fn.line2byte("$") == -1 then
+      require("nvim-tree.api").tree.open()
+    end
+  end,
+})
+
+-- NvimTree git colors
+vim.api.nvim_set_hl(0, "NvimTreeGitDirty", { fg = "#e06c75" })   -- red (unstaged)
+vim.api.nvim_set_hl(0, "NvimTreeGitStaged", { fg = "#98c379" })  -- green (staged)
+vim.api.nvim_set_hl(0, "NvimTreeGitNew", { fg = "#d19a66" })     -- orange (untracked)
+vim.api.nvim_set_hl(0, "NvimTreeGitDeleted", { fg = "#e06c75" }) -- red (deleted)
+vim.api.nvim_set_hl(0, "NvimTreeGitMerge", { fg = "#c678dd" })   -- purple (conflict)
+vim.api.nvim_set_hl(0, "NvimTreeGitRenamed", { fg = "#61afef" }) -- blue (renamed)
+vim.api.nvim_set_hl(0, "NvimTreeGitIgnored", { fg = "#5c6370" }) -- gray (ignored)
--- nvim/lua/plugins/init.lua
+++ nvim/lua/plugins/init.lua
@@ -19,6 +19,39 @@ return {
   -- { import = "nvchad.blink.lazyspec" },
 
   {
+    "lewis6991/gitsigns.nvim",
+    opts = {
+      current_line_blame = true,
+      current_line_blame_opts = {
+        delay = 200,
+        virt_text_pos = "eol",
+      },
+      current_line_blame_formatter = "<author>, <author_time:%R> - <summary>",
+      on_attach = function(bufnr)
+        local gs = require("gitsigns")
+        local map = vim.keymap.set
+
+        map("n", "]c", function()
+          if vim.wo.diff then return "]c" end
+          vim.schedule(function() gs.next_hunk() end)
+          return "<Ignore>"
+        end, { buffer = bufnr, expr = true, desc = "Next git hunk" })
+
+        map("n", "[c", function()
+          if vim.wo.diff then return "[c" end
+          vim.schedule(function() gs.prev_hunk() end)
+          return "<Ignore>"
+        end, { buffer = bufnr, expr = true, desc = "Prev git hunk" })
+
+        map("n", "<leader>ph", gs.preview_hunk, { buffer = bufnr, desc = "Preview hunk" })
+        map("n", "<leader>rh", gs.reset_hunk, { buffer = bufnr, desc = "Reset hunk" })
+        map("n", "<leader>rb", gs.reset_buffer, { buffer = bufnr, desc = "Reset buffer" })
+        map("n", "<leader>td", gs.toggle_deleted, { buffer = bufnr, desc = "Toggle deleted" })
+      end,
+    },
+  },
+
+  {
     "nvim-treesitter/nvim-treesitter",
   	opts = {
       ensure_installed = {
@@ -32,5 +65,42 @@ return {
         "gosum"
   	  },
   	},
+  },
+
+  -- GitHub Copilot
+  {
+    "zbirenbaum/copilot.lua",
+    cmd = "Copilot",
+    event = "InsertEnter",
+    opts = {
+      suggestion = {
+        enabled = true,
+        auto_trigger = true,
+        keymap = {
+          accept = "<Tab>",
+          accept_word = "<C-Right>",
+          accept_line = "<C-l>",
+          next = "<M-]>",
+          prev = "<M-[>",
+          dismiss = "<C-]>",
+        },
+      },
+      panel = { enabled = false },
+      filetypes = {
+        markdown = true,
+        help = false,
+      },
+    },
+  },
+
+  -- Remove Tab from cmp so Copilot can use it
+  {
+    "hrsh7th/nvim-cmp",
+    opts = function(_, opts)
+      opts.mapping["<Tab>"] = nil
+      opts.mapping["<S-Tab>"] = nil
+      opts.completion = { autocomplete = false }
+      return opts
+    end,
   },
 }
